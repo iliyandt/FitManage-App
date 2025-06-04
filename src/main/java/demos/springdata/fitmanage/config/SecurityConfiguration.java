ef7f41da6d6cec15b188143bc80325e766101a78
Iliyan Todorov <iliyan1298@icloud.com>
2025-06-04 23:03:44 +0300

    feat: Add Heroku app URL to the allowed origins list

--- src/main/java/demos/springdata/fitmanage/config/SecurityConfiguration.java
+++ src/main/java/demos/springdata/fitmanage/config/SecurityConfiguration.java
@@ -47,7 +47,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:8080", "https://git.heroku.com/fitmanage.git"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
