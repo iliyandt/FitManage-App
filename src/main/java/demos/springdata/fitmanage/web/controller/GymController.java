a818fb0fa8224598aacbb99fb803258000c59ba4
Iliyan Todorov <iliyan1298@icloud.com>
2025-06-06 15:48:29 +0300

    chore: add debug logs to inspect JWT principal in /me endpoint

--- src/main/java/demos/springdata/fitmanage/web/controller/GymController.java
+++ src/main/java/demos/springdata/fitmanage/web/controller/GymController.java
@@ -3,6 +3,8 @@ package demos.springdata.fitmanage.web.controller;
 import demos.springdata.fitmanage.domain.dto.GymAdminResponseDto;
 import demos.springdata.fitmanage.service.GymService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,20 @@ public class GymController {
     public GymController(GymService gymService) {
         this.gymService = gymService;
     }
+
+
+    @GetMapping("/me")
+    public ResponseEntity<GymAdminResponseDto> authenticatedGym() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+
+        System.out.println("Principal class: " + principal.getClass().getName());
+        System.out.println("Principal: " + principal.toString());
+
+        GymAdminResponseDto currentGym = (GymAdminResponseDto) authentication.getPrincipal();
+        return ResponseEntity.ok(currentGym);
+    }
+
 
     @GetMapping("/all")
     public ResponseEntity<List<GymAdminResponseDto>> getAllGyms() {
