7221a57f7bbac7d650f2573f2d58d6d06f3d9dd1
Iliyan Todorov <iliyan1298@icloud.com>
2025-08-25 17:26:13 +0300

    feat: add display name for role type enums

--- src/main/java/demos/springdata/fitmanage/domain/enums/RoleType.java
+++ src/main/java/demos/springdata/fitmanage/domain/enums/RoleType.java
@@ -1,10 +1,37 @@
 package demos.springdata.fitmanage.domain.enums;
 
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum RoleType {
-    SYSTEM_ADMIN,
-    FACILITY_ADMIN,
-    FACILITY_MEMBER,
-    FACILITY_STAFF
+    SYSTEM_ADMIN("System Admin"),
+    FACILITY_ADMIN("Facility Admin"),
+    FACILITY_MEMBER("Facility Member"),
+    FACILITY_STAFF("Facility Staff");
+
+    private final String displayName;
+
+    RoleType(String displayName) {
+        this.displayName = displayName;
+    }
+
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    @JsonCreator
+    public static RoleType fromString(String value) {
+        return RoleType.valueOf(value.toUpperCase());
+    }
+
+    @JsonValue
+    public String toJson() {
+        return displayName;
+    }
+
+
+
 }
 
