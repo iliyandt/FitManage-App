aa1c64f7ad93324c2b5ff40601034b29442aa584
Iliyan Todorov <iliyan1298@icloud.com>
2025-10-22 13:26:25 +0300

    employment enum: remove display name

--- src/main/java/demos/springdata/fitmanage/domain/enums/Employment.java
+++ src/main/java/demos/springdata/fitmanage/domain/enums/Employment.java
@@ -4,24 +4,10 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum Employment {
-    REGULAR("Regular"),
-    STUDENT("Student"),
-    SENIOR("Senior"),
-    HANDICAP("Handicap");
+    REGULAR,
+    STUDENT,
+    SENIOR,
+    HANDICAP
 
-    private final String displayName;
 
-    Employment(String displayName) {
-        this.displayName = displayName;
-    }
-
-    @JsonCreator
-    public static RoleType fromString(String value) {
-        return RoleType.valueOf(value.toUpperCase());
-    }
-
-    @JsonValue
-    public String toJson() {
-        return displayName;
-    }
 }
